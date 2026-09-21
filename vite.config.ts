362886c4918031885b9582b108add3151ed51bf5
xiaosebot <lvpin960628@gmail.com>
2026-09-21 18:48:12 +0800

    Update Pages base path for renamed repository

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/123pan-share-prototype/',
+  base: '/share-prototype/',
   plugins: [react()],
 });
